0eca1c80c914b64db2165eab0335d9fd354b8e17
ruhil <Jaiswal-ruhil>
2015-10-29 23:35:22 +0530

    completed basic functionality

--- resources/js/ramObj.js
+++ resources/js/ramObj.js
@@ -1,30 +1,46 @@
-
 //this object mimics the ram in a system
 
-var PhyMem = function( size ) {
+var Physical_Memory = function( size ) {
     this.size = size;
-    this._memory_ = [];
+    this.END = size-1;
+    this._memory_ = new Array(size);
 }
 
-PhyMem.prototype.insert = function( loc, value ) {
-    if( this.varifyLoc( loc ) )
-        this._memory_[loc] = value;
+Physical_Memory.prototype.insert = function( location, value ) {
+    if( this.varifyLoc( location ) )
+        this._memory_[location] = value & 0xff; //since sach location is just one byte
 }
 
-PhyMem.prototype.clearAll = function() {
-    this._memory_ = [];
+Physical_Memory.prototype.clearAll = function() {
+    this.clear( 0, this.END );
 }
 
-PhyMem.prototype.clear = function( loc ) {
-    if( this.varifyLoc( loc ) )
-        this._memory_[loc] = 0;
+Physical_Memory.prototype.clear = function( start_location, end_location ) {
+    if( this.varifyLoc(start_location) && this.varifyLoc(end_location) )
+        for(location=start_location; location<=end_location; location++)
+	    this._memory_[location] = 0;
+    else
+	this.errorHandler("invalid block")
 }
 
-PhyMem.prototype.varifyLoc = function( loc ) {
-    if( loc > this.size || loc < 0 )
+Physical_Memory.prototype.varifyLoc = function( location ) {
+    if( (location > this.END || location < 0) ) {
 	this.errorHandler( "INVALID_PHYSICAL_LOCATION" )
+    }
+    else
+	return true
 }
 
-PhyMem.prototype.errorHandler= function( errMsg ) {
+Physical_Memory.prototype.errorHandler= function( errMsg ) {
     console.log( errMsg )
 }
+
+Physical_Memory.prototype.fetch = function( location ) {
+    if( this.varifyLoc( location ) )
+	if( this._memory_[location] === undefined )
+	    this._memory_[location] =  Math.floor((Math.random() * 1000)%255);
+    return this._memory_[location];
+}
+
+//for testing under node js
+module.exports = Physical_Memory
